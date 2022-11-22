601300523a7f27d5ef2b5ceeabd934bfc7b42a90
lestone254 <lestonemandieka@gmail.com>
2022-11-22 11:57:21 +0300

    November 22 commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// script
+// Dismiss function for client buttons
+function dismiss() {
+    confirm("Dismiss writer?");
+
+    if (confirm.value=true) {
+        alert("Writer dismissed!")
+    }
+    else {
+        alert("Writer not dismissed!")
+    }
+    
+}
+// dismiss function for client buttons ends here
+
+// dismiss function for writers button begins here
+function wdismiss() {
+    confirm("Dismiss job?")
+}
+// dismiss function for writer button ends here
